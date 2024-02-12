2f5ea5f47ef9838deb6f032158372f6590a1b200
stephonfraserofficial <stephonfraserofficial@gmail.com>
2024-02-12 15:31:41 +0000

    Initial commit

---
+++ app/pages/signin.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const signin = () => {
+  return (
+    <div className='signin-body'>
+        <div className='left-container'></div>
+        <div className='right-container'></div>
+    </div>
+  )
+}
+
+export default signin
