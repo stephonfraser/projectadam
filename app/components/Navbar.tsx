ce1220079447a51e4a296b94140ad4bf68942eca
stephonfraserofficial <stephonfraserofficial@gmail.com>
2024-02-19 16:16:01 +0000

    Created navigation bar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import MenuItems from './MenuItems'
+import MenuMore from './MenuMore'
+import MenuProfile from './MenuProfile'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className=' flex navbar p-10 items-center w-full'>
+      <div className='logoname flex items-center space-x-4'>
+        <Link href={"/dashboard"}>
+          <Image src="/images/logo.png" alt="logo" width={50} height={50} style={{objectFit: "contain"}}	/>
+        </Link>
+        <div className='vt323-regular text-[30px] uppercase'>#PROJECTADAM</div>
+      </div>
+      <div className='menuItems ml-5 flex space-x-4 w-full justify-between items-center'>
+        <div className="itemsSection flex space-x-4 w-auto">
+          <MenuItems menuName='Dashboard' status='active'/>
+          <MenuItems menuName='Inventory' status='inactive'/>
+          <MenuItems menuName='Sales' status='inactive'/>
+          <MenuItems menuName='Customers' status='inactive'/>
+          <MenuItems menuName='Offers' status='inactive'/>
+          <MenuItems menuName='Receipts' status='inactive'/>
+          <MenuMore moreType='more'/>
+        </div>
+        <div className="profileSection flex space-x-4 justify-self-end ml-2.5 justify-between">
+          <MenuMore moreType='message'/>
+          <MenuMore moreType='notification'/>
+          {/* <MenuMore moreType='profile'/> */}
+          <MenuProfile />
+        </div>
+      </div>
+    </nav>
   )
 }
 
